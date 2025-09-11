6a40331148d9357ef981fbd87622a5d249511e99
ASWATHI M V <102293834+ASWATHIMV99@users.noreply.github.com>
2025-09-11 16:18:52 +0530

    Add files via upload

---
+++ pytest/run.sh
@@ -0,0 +1,4 @@
+cp /home/coder/project/workspace/pytest/tests.py /home/coder/project/workspace/Project/tests.py
+cd /home/coder/project/workspace/Project
+pip install pytest
+python3 -m pytest tests.py -v
